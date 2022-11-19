afb6e931c2d6b677fb0173444105dfe437bea715
SailorGa <ljia789@foxmail.com>
2022-11-19 17:59:12 +0800

    合并master 移除最大码率、起始码率、最小码率等编码参数 & 调整内部根据分辨率自动适配码率 & 修复部分手机因码率配置问题导致模糊问题 & 移除GPUImage的依赖 & 移除hkx.bundle资源文件 & 移除未使用的采集、渲染等 & 调整播放器中OpenGL创建绑定时机即：doInit调用时机，解决播放器初始化异常导致的持续黑屏幕 & 修复关闭摄像头入会时，横屏画面旋转异常问题

--- VCSSDK/Depend/hkScreenShared.framework/Headers/hkScreenShared.h
+++ VCSSDK/Depend/hkScreenShared.framework/Headers/hkScreenShared.h
@@ -7,15 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
-#define SCREEENVERSION @"9.4.13@20220623"
+
+#define SCREEENVERSION @"9.5.0@2022.11.19"
+
 //! Project version number for hkScreenShared.
 FOUNDATION_EXPORT double hkScreenSharedVersionNumber;
 
 //! Project version string for hkScreenShared.
 FOUNDATION_EXPORT const unsigned char hkScreenSharedVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <hkScreenShared/PublicHeader.h>
-
 #import<hkScreenShared/ScreenRTCClient.h>
 #import<hkScreenShared/ScreenRTCServer.h>
+
+/// In this header, you should import all the public headers of your framework using statements like #import <hkScreenShared/PublicHeader.h>
+
 
