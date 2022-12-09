f11b83303a183034695909638a07d7f293ad1d70
SailorGa <ljia789@foxmail.com>
2022-12-09 13:37:01 +0800

    合并master分支 修复流媒体视频发送状态导致的共享流接收异常问题

--- VCSSDK/Depend/VCSSDK.framework/PrivateHeaders/VCSMeetingManager.h
+++ VCSSDK/Depend/VCSSDK.framework/PrivateHeaders/VCSMeetingManager.h
@@ -65,6 +65,8 @@ typedef void (^VCSMeetingManagerDestroyBlock)(void);
 @property (nonatomic, assign) BOOL isCameraCapture;
 #pragma mark 标识当前是否在房间内
 @property (nonatomic, assign) BOOL enterRoom;
+#pragma mark 标记本地共享状态
+@property (nonatomic, assign) BOOL isLocalShared;
 
 #pragma mark 标记是否开启语音模式(YES-开启 NO-关闭)
 /// 标记是否开启语音模式(YES-开启 NO-关闭)
